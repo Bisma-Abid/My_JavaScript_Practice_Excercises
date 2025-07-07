2b72987acd9fa34aa1b4ec20404181a28befd7c9
Bisma <bismaabid174@gmail.com>
2025-07-07 14:00:24 +0500

    Add files via upload

---
+++ ex#7/data.js
@@ -0,0 +1,9 @@
+let num1 = 2.8 ;
+
+let round1 = Math.round(num1);
+
+let num2 = 2.2 ;
+
+let round2 = Math.round(num2);
+
+document.getElementById("total").innerHTML = "Round 2.8 = " + round1 + "<br> Round 2.2 =" + round2;
